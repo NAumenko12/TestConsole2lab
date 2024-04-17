8bbd984c10a3d703c01981ffff8b1934592a26c2
NAumenko12 <143539153+NAumenko12@users.noreply.github.com>
2024-04-17 10:13:54 +0700

    Update Program.cs

--- TestConsole/Program.cs
+++ TestConsole/Program.cs
@@ -2,29 +2,69 @@
 {
     internal class Program
     {
-        static void Main(string[] args)
+       static void Main(string[] args)
         {
-            Console.WriteLine("Добрый день");
-            Console.WriteLine("Введите число 1");
-            int FirstNumber =Convert.ToInt32(Console.ReadLine());
-            TestInput("первое число ", FirstNumber);
-            Console.WriteLine("Введите число 2");
-            int SecondNumber = Convert.ToInt32(Console.ReadLine());
-            TestInput("второе число ", SecondNumber);
-            Console.WriteLine ("Матрица чисел: " + FirstNumber+" "+SecondNumber);
-            for (int i = 1; i <= FirstNumber; i++)
+            try
             {
-                for(int j = 1; j <= SecondNumber; j++)
+                Console.WriteLine("Добрый день");
+                Console.WriteLine("Введите число 1");
+                string input1 = Console.ReadLine();
+                if (!IsNumberValid(input1))
                 {
-                    Console.WriteLine(i * j );
+                    throw new ArgumentException("Введено неверное значение для числа 1");
                 }
-                Console.WriteLine();
+                double FirstNumber;
+                if (!double.TryParse(input1, out FirstNumber))
+                {
+                    throw new ArgumentException("Введено неверное значение для числа 1");
+                }
+                Console.WriteLine("Введите число 2");
+                string input2 = Console.ReadLine();
+                if (!IsNumberValid(input2))
+                {
+                    throw new ArgumentException("Введено неверное значение для числа 2");
+                }
+                double SecondNumber;
+                if (!double.TryParse(input2, out SecondNumber))
+                {
+                    throw new ArgumentException("Введено неверное значение для числа 2");
+                }
+               TestInput("первое число ", FirstNumber);
+                TestInput("второе число ", SecondNumber);
+                Console.WriteLine("Матрица чисел: " + FirstNumber + " " + SecondNumber);
+                for (int i = 1; i <= FirstNumber; i++)
+                {
+                    for (int j = 1; j <= SecondNumber; j++)
+                    {
+                        Console.WriteLine(i * j);
+                    }
+                    Console.WriteLine();
+                }
             }
-
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"Ошибка: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Произошла ошибка: {e.Message}");
+            }
         }
-        static void TestInput(string virable , int value)
+        static void TestInput(string virable, double value)
         {
             Console.WriteLine($"Вы ввели {virable} со значением {value}");
         }
+        static bool IsNumberValid(string input)
+        {
+            foreach (char c in input)
+            {
+                if (!char.IsDigit(c) && c != '.' && c != ',' && c != '%' && c != '*' && c != '$' && c != '?')
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
     }
 }
